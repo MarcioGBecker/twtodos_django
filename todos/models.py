56c080a2e5460ed981b0309384c1cb0309d609b6
MarcioGBecker <mrcbecker1@gmail.com>
2024-01-30 21:13:57 -0300

    Migrando informacoes para o banco de dados

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(
+        max_length=100, null=False, blank=False
+    )  # Titulo da tarefa
+    created_at = models.DateTimeField(
+        auto_now_add=True, null=False, blank=False
+    )  # Data em que a tarefa foi criada
+    deadLine = models.DateTimeField(
+        null=False, blank=False
+    )  # Prazo para encerrar a tarefa
+    finished_at = models.DateTimeField(null=True)  # Data real de encerramento da tarefa
