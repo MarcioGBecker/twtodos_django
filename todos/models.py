27411e34c1dbc2d17a63e1a15282cfaba3e6e333
MarcioGBecker <mrcbecker1@gmail.com>
2024-02-02 00:15:56 -0300

    Criação da página de cadastros de tarefas, utilização de CRUD, reaproveitamento de código e remoção de bugs

--- todos/models.py
+++ todos/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(
+    title = models.CharField(verbose_name="Título",
         max_length=100, null=False, blank=False
     )  # Titulo da tarefa
     created_at = models.DateTimeField(
         auto_now_add=True, null=False, blank=False
     )  # Data em que a tarefa foi criada
-    deadLine = models.DateField(
+    deadLine = models.DateField(verbose_name="Data de entrega",
         null=False, blank=False
     )  # Prazo para encerrar a tarefa
     finished_at = models.DateField(null=True)  # Data real de encerramento da tarefa
